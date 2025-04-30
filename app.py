1a130867771b13a45c8ebfd68345d2bdaf85c5c8
Devanshji <devanshgoyal7344@gmail.com>
2025-05-01 00:22:46 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import pickle
+from sklearn.preprocessing import LabelEncoder
 
 # Load model
 model = pickle.load(open('insurancemodelf.pkl', 'rb'))
@@ -16,16 +17,31 @@ children = st.number_input("Number of Children", min_value=0, max_value=10)
 smoker = st.selectbox("Smoker", ["yes", "no"])
 region = st.selectbox("Region", ["southeast", "southwest", "northeast", "northwest"])
 
-# Predict button
+# Initialize label encoders for categorical columns
+sex_encoder = LabelEncoder()
+smoker_encoder = LabelEncoder()
+region_encoder = LabelEncoder()
+
+# Train the encoder on the unique values that you expect to encounter.
+sex_encoder.fit(["male", "female"])
+smoker_encoder.fit(["yes", "no"])
+region_encoder.fit(["southeast", "southwest", "northeast", "northwest"])
+
+# Prediction logic on button click
 if st.button("Predict Insurance Cost"):
-    # Create a DataFrame with the correct column names
+    # Encode categorical variables
+    encoded_sex = sex_encoder.transform([sex])[0]
+    encoded_smoker = smoker_encoder.transform([smoker])[0]
+    encoded_region = region_encoder.transform([region])[0]
+
+    # Create DataFrame for prediction with encoded variables
     input_df = pd.DataFrame([{
         'age': age,
-        'sex': sex,
+        'sex': encoded_sex,
         'bmi': bmi,
         'children': children,
-        'smoker': smoker,
-        'region': region
+        'smoker': encoded_smoker,
+        'region': encoded_region
     }])
 
     # Make prediction
